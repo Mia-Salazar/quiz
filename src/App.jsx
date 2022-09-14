cc2ddbfb88a9b4f49c78c98381cb5a8434e12368
Mia-Salazar <maria.sgr@gmail.com>
2022-09-14 18:54:56 +0200

    Add home and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.scss';
+import Navbar from './app/components/Navbar';
 import AppContext from './app/context/AppContext';
 import useInitialState from './app/hooks/useInitialState';
+import Home from './app/page/Home';
 
 function App() {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-      <main className="tui-bg-blue-black main">
-      </main>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+      </BrowserRouter>
     </AppContext.Provider>
 
   );
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+  return (
+    <header className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <div className="container">
+        <a href="../" className="navbar-brand">Mass Effect Quiz</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <nav className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav">
+          <li className="nav-item">
+          <Link className="nav-link"  to="/">Quiz</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/ranking" className="nav-link">Ranking</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
