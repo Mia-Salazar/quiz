c4dfb1831ab3bae33cd5d38a118784ce74d62b83
Mia-Salazar <maria.sgr@gmail.com>
2022-09-14 19:56:01 +0200

    Add questions

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <header className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
       <div className="container">
-        <a href="../" className="navbar-brand">Mass Effect Quiz</a>
+        <p className="navbar-brand">Mass Effect Quiz</p>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
