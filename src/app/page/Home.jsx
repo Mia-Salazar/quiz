231d333115dcb003b4b2f0247c56d074fac35fdb
Mia-Salazar <maria.sgr@gmail.com>
2022-09-19 11:29:46 +0200

    Add question container

--- src/app/page/Home.jsx
+++ src/app/page/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Question from '../components/Question';
 import Time from '../components/Time';
+import { quizQuestions } from '../utils/quizQuestions';
+
 
 const Home = () => {
-
+    const [number, setNumber] = useState(1);
 
   return (
     <main>
         <Time />
+        <Question number={number} question={quizQuestions[0]}/>
     </main>
   );
 }
